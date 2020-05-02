d4e995b9c34fd4970f7459142175224844c57666
Tobi Tselios <tobi.tselios@gmail.com>
2020-05-02 22:56:49 +0100

    Storyboard Setup and View Controllers Created

---
+++ GithubCommits/DetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DetailViewController.swift
+//  GithubCommits
+//
+//  Created by Tobi Kuyoro on 02/05/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
