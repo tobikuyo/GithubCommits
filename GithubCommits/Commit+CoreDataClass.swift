a041ef3582e1daa9e9149d29f826d7d1425ba870
Tobi Tselios <tobi.tselios@gmail.com>
2020-05-03 14:55:27 +0100

    Created an NSMangedObject Subclass

---
+++ GithubCommits/Commit+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Commit+CoreDataClass.swift
+//  GithubCommits
+//
+//  Created by Tobi Kuyoro on 03/05/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Commit)
+public class Commit: NSManagedObject {
+
+}
---
+++ GithubCommits/Commit+CoreDataProperties.swift
@@ -0,0 +1,23 @@
+//
+//  Commit+CoreDataProperties.swift
+//  GithubCommits
+//
+//  Created by Tobi Kuyoro on 03/05/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+extension Commit {
+
+    @nonobjc public class func createFetchRequest() -> NSFetchRequest<Commit> {
+        return NSFetchRequest<Commit>(entityName: "Commit")
+    }
+
+    @NSManaged public var date: Date
+    @NSManaged public var message: String
+    @NSManaged public var sha: String
+    @NSManaged public var url: String
+}
